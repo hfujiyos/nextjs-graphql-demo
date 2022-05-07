42f97d186b6e800c26a1948a817d8056989391de
hfujiyos <hfujiyos@gmail.com>
2022-05-07 14:02:04 +0900

    設計書更新

--- __tests__/Home.test.tsx
+++ __tests__/Home.test.tsx
@@ -6,6 +6,7 @@ import { render, screen } from '@testing-library/react'
 import '@testing-library/jest-dom/extend-expect'
 import Home from '../pages/index'
 
+/** ﾃｽﾄｺｰﾄﾞ｜Homeｺﾝﾎﾟｰﾈﾝﾄをﾚﾝﾀﾞﾘﾝｸﾞして指定ﾃｷｽﾄを読取できればﾃｽﾄ成功 */
 it('Should render title text', () => {
   if (typeof window === 'object') {
     render(<Home />)
